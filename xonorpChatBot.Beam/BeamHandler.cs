4726f9b0cc32b3f68c3cbb0a11a2fdd9ac36596d
Tim Van Den Bosch Phoenix <tim_vandenbosch@msn.com>
2017-03-22 14:44:37 +0100

    [ADD] Beam
    Empty Beam project

---
+++ xonorpChatBot.Beam/BeamHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xonorpChatBot.Beam
+{
+    class BeamHandler
+    {
+        public string HelloWorld()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
